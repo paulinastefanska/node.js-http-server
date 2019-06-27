6a8a9875b0ac42042ab3e638a1ee32a8d7753259
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-27 15:42:25 +0200

    Add fs.readFile and 404 img

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+var fs = require("fs");
 var http = require('http');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
+
     if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
+        fs.readFile("index.html", "utf-8", function(err, data) {
+	      if (err) throw err;
+	      response.write(data);
+	      response.end();
+	    });
     } else {
             response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.write('<img src="https://image.freepik.com/darmowe-wektory/zabawny-blad-404-projekt-tla_1167-219.jpg" alt="404">');
             response.end();
     }
 });
