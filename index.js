d2399ca2f39347218035c06a1f1af479549e2a8a
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-27 15:11:02 +0200

    Test stream

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 var server = http.createServer();
 server.on('request', function (request, response) {
     response.write('Hello world!');
-    response.end();
+	response.end();
 });
 server.listen(9000);
